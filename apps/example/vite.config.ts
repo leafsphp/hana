bdc51be4354c4ab3e28a13ec77019f5bfa443271
mychidarko <mickdd22@gmail.com>
2023-11-26 08:49:20 +0000

    chore: update example app

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
 import { hana } from '@hanabira/router';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   plugins: [
     react(),
     hana({
