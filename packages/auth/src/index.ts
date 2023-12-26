30d04e21ddf060bed961d43f7ba25a69658539e0
mychidarko <mickdd22@gmail.com>
2023-12-26 01:18:54 +0000

    feat: add getAuth method

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,5 +1,5 @@
 export * from './@types/core';
 
-export { createAuth } from './core/manager';
+export { createAuth, getAuth } from './core/manager';
 export { useAuthState, useGuestState, useLogin, useLogout } from './core/hooks';
 export { withAuthState, withGuestState } from './core/hoc';
