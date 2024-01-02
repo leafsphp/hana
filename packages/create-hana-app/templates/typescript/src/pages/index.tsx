3b6c0322b06d6705db0b37be5e58a21b4f95b6ae
Master Sofua Desmond <mrsofua@gmail.com>
2024-01-02 13:18:43 +0000

    chore: update starter template

--- packages/create-hana-app/templates/typescript/src/pages/index.tsx
+++ packages/create-hana-app/templates/typescript/src/pages/index.tsx
@@ -1,18 +1,34 @@
-import { useNavigate } from '@hanabira/router';
+import styles from './index.module.css';
 
-const Home = () => {
-  const navigate = useNavigate();
-
+export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>This content can be found in pages/index.tsx</p>
+    <main className={styles.main}>
+      <div className={styles.row} style={{ marginLeft: 'auto' }}>
+        <p>BY</p>
+        <img
+          src="https://www.leafphp.dev/logo-circle.png"
+          width={39}
+          alt=""
+          className="ms-10"
+        />
+        <p style={{ fontWeight: 700 }} className="ms-5">
+          Leaf
+        </p>
+      </div>
+      <div className={styles.center}>
+        <h1>HANA</h1>
+        <p>Lightweight React for Humans</p>
+        <p className={styles.code}>
+          Get started by editing
+          <code className={styles.codespan}> pages/index.tsx</code>
+        </p>
+      </div>
 
-      <button onClick={() => navigate('/dashboard')}>
-        Navigate to /dashboard
-      </button>
-    </div>
+      <div className={styles.row} style={{ gap: 50 }}>
+        <a className={styles.button} href="https://hanajs.dev/" target="_blank">
+          Docs
+        </a>
+      </div>
+    </main>
   );
-};
-
-export default Home;
+}
