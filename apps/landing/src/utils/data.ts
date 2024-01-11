31b5a6e26d5be4dc27e9d865c5819d9f53e40137
Master Sofua Desmond <mrsofua@gmail.com>
2024-01-11 21:55:31 +0000

    feat: add landing page with astro

---
+++ apps/landing/src/utils/data.ts
@@ -0,0 +1,55 @@
+export const SPONSORS = [
+  {
+    image: '/leaf.png',
+    alt: 'Leaf PHP',
+    href: 'https://leafphp.dev/',
+  },
+  {
+    image: '/digitalocean.png',
+    alt: 'Digital Ocean',
+    href: 'https://www.digitalocean.com/',
+  },
+  {
+    image: '/jetbrains.png',
+    alt: 'JetBrains',
+    href: 'https://www.jetbrains.com/',
+  },
+  {
+    image: '/netlify.png',
+    alt: 'Netlify',
+    href: 'https://netlify.com/',
+  },
+];
+
+export const FEEDBACK = [
+  {
+    user: {
+      image: '/leaf.png',
+      name: 'Desmond Sofua',
+      position: 'Founder',
+      company: 'MailPigeon',
+    },
+    feedback:
+      '“With Next.js at the helm of our headless tech stack, our developers can create features with velocity and speed, ultimately enabling users to create whatever, whenever they want to.”',
+  },
+  {
+    user: {
+      image: '/leaf.png',
+      name: 'Desmond Sofua',
+      position: 'Founder',
+      company: 'MailPigeon',
+    },
+    feedback:
+      '“With Next.js at the helm of our headless tech stack, our developers can create features with velocity and speed, ultimately enabling users to create whatever, whenever they want to.”',
+  },
+  {
+    user: {
+      image: '/leaf.png',
+      name: 'Desmond Sofua',
+      position: 'Founder',
+      company: 'MailPigeon',
+    },
+    feedback:
+      '“With Next.js at the helm of our headless tech stack, our developers can create features with velocity and speed, ultimately enabling users to create whatever, whenever they want to.”',
+  },
+];
