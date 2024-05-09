2f3455f7df7f81db19d980442b69aea7ab871579
Desmond Sofua <mrsofua@gmail.com>
2024-05-09 10:31:53 +0000

    fix: resolve alias from vite config

--- packages/create-hana-app/templates/typescript/vite.config.ts
+++ packages/create-hana-app/templates/typescript/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import { hana } from '@hanabira/router';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   plugins: [
     react(),
     hana({
