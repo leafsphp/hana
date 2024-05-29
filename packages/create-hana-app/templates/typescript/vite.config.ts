99f22d76c22154607e2e23cb893b95f747ab3bac
Desmond Sofua <mrsofua@gmail.com>
2024-05-29 10:11:53 +0000

    refactor: handle paths with vite plugin

--- packages/create-hana-app/templates/typescript/vite.config.ts
+++ packages/create-hana-app/templates/typescript/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { hana } from '@hanabira/router';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   plugins: [
+    tsconfigPaths(),
     react(),
     hana({
       root: __dirname,
